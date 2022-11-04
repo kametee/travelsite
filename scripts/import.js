3f9d57f6203a06f19ee4c6f6e1e0f22fcf9d053c
keigo yoneda <mkkvr57@gmail.com>
2022-11-04 13:17:31 +0900

    now

---
+++ scripts/import.js
@@ -0,0 +1,9 @@
+
+document.write("<script src='vendors/scroll-polyfill.js'>");
+document.write("<script src='vendors/TweenMax.min.js'>");
+document.write("<script src='vendors/swiper.min.js'>");
+document.write("<script src='libs/scroll.js'>");
+document.write("<script src='libs/text-animation.js'>");
+document.write("<script src='libs/hero-slider.js'>");
+document.write("<script src='libs/mobile-menu.js'>");
+document.write("<script src='main.js'>");
